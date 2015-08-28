629e6849a4938cdd171b5942a5df91ec92469c68
Charlie Schwabacher <charlieschwabacher@gmail.com>
2015-08-28 10:45:23 -0700

    Update todo_type.rb

--- app/graph/types/todo_type.rb
+++ app/graph/types/todo_type.rb
@@ -1,39 +1,3 @@
-# var {nodeInterface, nodeField} = nodeDefinitions(
-#   (globalId) => {
-#     var {type, id} = fromGlobalId(globalId);
-#     if (type === 'Todo') {
-#       return getTodo(id);
-#     } else if (type === 'User') {
-#       return getUser(id);
-#     }
-#     return null;
-#   },
-#   (obj) => {
-#     if (obj instanceof Todo) {
-#       return GraphQLTodo;
-#     } else if (obj instanceof User) {
-#       return GraphQLUser;
-#     }
-#     return null;
-#   }
-# );
-
-# var GraphQLTodo = new GraphQLObjectType({
-#   name: 'Todo',
-#   fields: {
-#     id: globalIdField('Todo'),
-#     text: {
-#       type: GraphQLString,
-#       resolve: (obj) => obj.text,
-#     },
-#     complete: {
-#       type: GraphQLBoolean,
-#       resolve: (obj) => obj.complete,
-#     }
-#   },
-#   interfaces: [nodeInterface]
-# });
-
 TodoType = GraphQL::ObjectType.define do
   name "Todo"
   description "An item in a todo list"
